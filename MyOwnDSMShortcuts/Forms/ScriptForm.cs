e4d27301ae1334095fc2fb7065bd41e6257bce08
vletroye <vletroye@gmail.com>
2018-01-02 22:20:44 +0100

    Replace local projet ScintallaNET with NugGet Package

--- MyOwnDSMShortcuts/Forms/ScriptForm.cs
+++ MyOwnDSMShortcuts/Forms/ScriptForm.cs
@@ -1,14 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using ScintillaNET;
 using System.Diagnostics;
+using ScintillaNET;
 
 namespace BeatificaBytes.Synology.Mods
 {
